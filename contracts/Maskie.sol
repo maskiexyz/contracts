94f6810594bb9c2ccdb605b13a85c205c5c68822
wd021 <dannywitters@gmail.com>
2023-08-21 08:48:34 +0200

    add accesscontrol

--- contracts/Maskie.sol
+++ contracts/Maskie.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Maskie is OwnableUpgradeable, ERC721Upgradeable {
+contract Maskie is OwnableUpgradeable, ERC721Upgradeable, AccessControlUpgradeable {
     using Strings for uint256;
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     string public baseURI;
     address public usdcAddress;
@@ -25,7 +27,10 @@ contract Maskie is OwnableUpgradeable, ERC721Upgradeable {
 
     function initialize() public initializer {
         __ERC721_init("Maskie", "MASK");
-        __Ownable_init_unchained();
+        __Ownable_init();
+        __AccessControl_init();
+
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
 
         protocolFeePercentage = 5;
         distributionFeePercentage = 10;
@@ -64,7 +69,7 @@ contract Maskie is OwnableUpgradeable, ERC721Upgradeable {
         distributionFeePercentage = _percentage;
     }
 
-    function mint(uint256 id, address _creator, address _owner, uint256 price) external onlyOwner {
+    function mint(uint256 id, address _creator, address _owner, uint256 price) external onlyRole(ADMIN_ROLE) {
         uint256 protocolFee = (price * protocolFeePercentage) / 100;
         uint256 creatorPayment = price - protocolFee;
 
@@ -77,7 +82,7 @@ contract Maskie is OwnableUpgradeable, ERC721Upgradeable {
         emit MaskieMinted(id, _creator, _owner, price);
     }
 
-    function buy(uint256 id, uint256 price, address _newOwner) external onlyOwner {
+    function buy(uint256 id, uint256 price, address _newOwner) external onlyRole(ADMIN_ROLE) {
         address previousOwner = ownerOf(id);
         require(previousOwner != address(0), 'Token does not exist');
 
@@ -94,13 +99,29 @@ contract Maskie is OwnableUpgradeable, ERC721Upgradeable {
         emit MaskieBought(id, previousOwner, _newOwner, price);
     }
 
-    function distribute(address[] memory recipients, uint256[] memory amounts) external onlyOwner {
+    function distribute(address[] memory recipients, uint256[] memory amounts) external onlyRole(ADMIN_ROLE) {
         require(recipients.length == amounts.length, "Mismatched input arrays");
         
         for (uint256 i = 0; i < recipients.length; i++) {
             require(IERC20(usdcAddress).transferFrom(distributionFeeAddress, recipients[i], amounts[i]), "Reward distribution failed");
             emit RewardsDistributed(recipients[i], amounts[i]);
         }
+    }
+
+    function grantAdminRole(address account) public onlyOwner {
+        grantRole(ADMIN_ROLE, account);
+    }
+
+    function revokeAdminRole(address account) public onlyOwner {
+        revokeRole(ADMIN_ROLE, account);
+    }
+
+    function isAdmin(address account) public view returns (bool) {
+        return hasRole(ADMIN_ROLE, account);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Upgradeable, AccessControlUpgradeable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
     function withdraw() public onlyOwner {
